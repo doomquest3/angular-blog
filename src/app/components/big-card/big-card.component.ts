757fc312e6f6b1e34adcf5b8b908abe4c41dc479
Leandro de Jesus <lean_silva100@hotmail.com>
2023-05-30 19:49:35 -0300

    3º commit, components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+
+  @Input() photoCover:string = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg";
+  @Input() cardTitle:string = "";
+  @Input() cardDescription: string = "";
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
